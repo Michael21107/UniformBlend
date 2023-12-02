8917279d9f3b1d7cb969c4c51f04795177cb5586
Haze <115841732+HazeStudioFR@users.noreply.github.com>
2023-12-01 20:50:45 -0500

    Initial commit

---
+++ PasteToLine/Outfit.cs
@@ -0,0 +1,176 @@
+﻿using System;
+
+namespace UniformBlend
+{
+    internal class Outfit
+    {
+        public string Name { get; set; }
+        public string Gender { get; set; }
+        public Component Hat { get; set; }
+        public Component Glasses { get; set; }
+        public Component Ear { get; set; }
+        public Component Watch { get; set; }
+        public Component Mask { get; set; }
+        public Component Top { get; set; }
+        public Component UpperSkin { get; set; }
+        public Component Decal { get; set; }
+        public Component UnderCoat { get; set; }
+        public Component Pants { get; set; }
+        public Component Shoes { get; set; }
+        public Component Accessories { get; set; }
+        public Component Armor { get; set; }
+        public Component Parachute { get; set; }
+
+        public Outfit()
+        {
+            Hat = new Component();
+            Glasses = new Component();
+            Ear = new Component();
+            Watch = new Component();
+            Mask = new Component();
+            Top = new Component();
+            UpperSkin = new Component();
+            Decal = new Component();
+            UnderCoat = new Component();
+            Pants = new Component();
+            Shoes = new Component();
+            Accessories = new Component();
+            Armor = new Component();
+            Parachute = new Component();
+        }
+
+        public class Component
+        {
+            public int Number { get; set; }
+            public int Texture { get; set; }
+
+            public override string ToString()
+            {
+                return $"{Number}:{Texture}";
+            }
+        }
+
+        public void SetComponent(string componentName, int number, int texture)
+        {
+            switch(componentName.ToLower())
+            {
+                case "hat":
+                    Hat.Number = Math.Max(0, number - 1);
+                    Hat.Texture = Math.Max(0, texture - 1);
+                    break;
+
+                case "glasses":
+                    Glasses.Number = Math.Max(0, number - 1);
+                    Glasses.Texture = Math.Max(0, texture - 1);
+                    break;
+
+                case "ear":
+                    Ear.Number = Math.Max(0, number - 1);
+                    Ear.Texture = Math.Max(0, texture - 1);
+                    break;
+
+                case "watch":
+                    Watch.Number = Math.Max(0, number - 1);
+                    Watch.Texture = Math.Max(0, texture - 1);
+                    break;
+
+                case "mask":
+                    Mask.Number = Math.Max(0, number - 1);
+                    Mask.Texture = Math.Max(0, texture - 1);
+                    break;
+
+                case "top":
+                    Top.Number = Math.Max(0, number - 1);
+                    Top.Texture = Math.Max(0, texture - 1);
+                    break;
+
+                case "upperskin":
+                    UpperSkin.Number = Math.Max(0, number - 1);
+                    UpperSkin.Texture = Math.Max(0, texture - 1);
+                    break;
+
+                case "decal":
+                    Decal.Number = Math.Max(0, number - 1);
+                    Decal.Texture = Math.Max(0, texture - 1);
+                    break;
+
+                case "undercoat":
+                    UnderCoat.Number = Math.Max(0, number - 1);
+                    UnderCoat.Texture = Math.Max(0, texture - 1);
+                    break;
+
+                case "pants":
+                    Pants.Number = Math.Max(0, number - 1);
+                    Pants.Texture = Math.Max(0, texture - 1);
+                    break;
+
+                case "shoes":
+                    Shoes.Number = Math.Max(0, number - 1);
+                    Shoes.Texture = Math.Max(0, texture - 1);
+                    break;
+
+                case "accessories":
+                    Accessories.Number = Math.Max(0, number - 1);
+                    Accessories.Texture = Math.Max(0, texture - 1);
+                    break;
+
+                case "armor":
+                    Armor.Number = Math.Max(0, number - 1);
+                    Armor.Texture = Math.Max(0, texture - 1);
+                    break;
+
+                case "parachute":
+                    Parachute.Number = Math.Max(0, number - 1);
+                    Parachute.Texture = Math.Max(0, texture - 1);
+                    break;
+            }
+        }
+
+        public static Outfit ParseOutfit(string input)
+        {
+            Outfit outfit = new Outfit();
+
+            string[] lines = input.Split('\n', (char)StringSplitOptions.RemoveEmptyEntries);
+
+            foreach(string line in lines)
+            {
+                if(line.StartsWith("[") && line.EndsWith("]"))
+                {
+                    outfit.Name = line.Trim('[', ']').Trim();
+                }
+                else
+                {
+                    string[] parts = line.Split('=');
+                    if(parts.Length == 2)
+                    {
+                        string propertyName = parts[0].Trim().ToLower();
+                        string propertyValue = parts[1].Trim();
+
+                        if(propertyName == "gender")
+                        {
+                            outfit.Gender = propertyValue.ToLower();
+                            continue; 
+                        }
+
+                        string[] values = propertyValue.Split(':');
+
+                        if(values.Length == 2 && int.TryParse(values[0], out int number) && int.TryParse(values[1], out int texture))
+                        {
+                            outfit.SetComponent(propertyName, number, texture);
+                        }
+                        else
+                        {
+                            Console.WriteLine($"Invalid input: {line}");
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Invalid input: {line}");
+                    }
+                }
+            }
+
+            return outfit;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,119 @@
+﻿using System.Collections.Generic;
+using System.Threading;
+using static System.Console;
+
+namespace UniformBlend
+{
+    internal class Program
+    {
+        private static void Main()
+        {
+            WriteLine("Welcome to UniformBlend!");
+            WriteLine("------------------------------");
+            WriteLine("1. Paste To Line");
+            WriteLine("2. Credits");
+            WriteLine("------------------------------");
+            Write("Please enter your choice (1, or 2): ");
+            WriteLine();
+
+            string userInput = ReadLine();
+
+            switch(userInput)
+            {
+                case "1":
+                    PasteToLine();
+                    break;
+
+                case "2":
+                    Clear();
+                    WriteLine("Haze/HazeStudioFR");
+                    break;
+
+                default:
+                    WriteLine("Invalid choice. Please enter 1, 2, or 3.");
+                    break;
+            }
+            ReadLine();
+        }
+
+        private static void PasteToLine()
+        {
+            Clear();
+            WriteLine("You Have Selected Paste To Line");
+            List<string> userInputLines = new List<string>();
+            WriteLine("Paste the lines of information. Enter an empty line to finish.");
+            WriteLine("------------------------------");
+            WriteLine();
+
+            while(true)
+            {
+                string line = ReadLine();
+
+                if(string.IsNullOrWhiteSpace(line))
+                {
+                    break;
+                }
+
+                userInputLines.Add(line);
+            }
+
+            string newString = string.Join("\n", userInputLines);
+            Outfit outfit = Outfit.ParseOutfit(newString);
+
+            WriteLine("The Following Has Been Inputed.");
+            WriteLine("------------------------------");
+            WriteLine($"Outfit Name: {outfit.Name}");
+            WriteLine($"Outfit Gender: {outfit.Gender}");
+            WriteLine($"Outfit Hat: {outfit.Hat}");
+            WriteLine($"Outfit Glasses: {outfit.Glasses}");
+            WriteLine($"Outfit Ear: {outfit.Ear}");
+            WriteLine($"Outfit Watch: {outfit.Watch}");
+            WriteLine($"Outfit Mask: {outfit.Mask}");
+            WriteLine($"Outfit Top: {outfit.Top}");
+            WriteLine($"Outfit UpperSkin: {outfit.UpperSkin}");
+            WriteLine($"Outfit Decal: {outfit.Decal}");
+            WriteLine($"Outfit UnderCoat: {outfit.UnderCoat}");
+            WriteLine($"Outfit Pants: {outfit.Pants}");
+            WriteLine($"Outfit Shoes: {outfit.Shoes}");
+            WriteLine($"Outfit Accessories: {outfit.Accessories}");
+            WriteLine($"Outfit Armor: {outfit.Armor}");
+            WriteLine($"Outfit Parachute: {outfit.Parachute}");
+            WriteLine("------------------------------");
+
+            Write("Converting To LSPDFR Format...");
+            using(var progress = new ProgressBar())
+            {
+                for(int i = 0; i <= 100; i++)
+                {
+                    progress.Report((double)i / 100);
+                    Thread.Sleep(10);
+                }
+            }
+            WriteLine("Done.");
+            Thread.Sleep(2000);
+            Clear();
+
+            WriteLine("Please Enter ScriptName. Ex m_base");
+            string ScriptName = ReadLine();
+            Clear();
+            WriteLine("<Variation>");
+            WriteLine($"<Name>{outfit.Name}</Name>");
+            WriteLine($"<ScriptName>{ScriptName}</ScriptName>");
+            WriteLine($"<Gender>{outfit.Gender.ToLower()}</Gender>");
+            WriteLine($"<Components>");
+            WriteLine($"<Component id=\"3\" drawable=\"{outfit.UpperSkin.Number}\" texture=\"{outfit.UpperSkin.Texture}\" />");
+            WriteLine($"<Component id=\"4\" drawable=\"{outfit.Pants.Number}\" texture=\"{outfit.Pants.Texture}\" />");
+            WriteLine($"<Component id=\"5\" drawable=\"{outfit.Parachute.Number}\" texture=\"{outfit.Parachute.Texture}\" />");
+            WriteLine($"<Component id=\"6\" drawable=\"{outfit.Shoes.Number}\" texture=\"{outfit.Shoes.Texture}\" />");
+            WriteLine($"<Component id=\"7\" drawable=\"{outfit.Accessories.Number}\" texture=\"{outfit.Accessories.Texture}\" />");
+            WriteLine($"<Component id=\"8\" drawable=\"{outfit.UnderCoat.Number}\" texture=\"{outfit.UnderCoat.Texture}\" />");
+            WriteLine($"<Component id=\"9\" drawable=\"{outfit.Armor.Number}\" texture=\"{outfit.Armor.Texture}\" />");
+            WriteLine($"<Component id=\"10\" drawable=\"{outfit.Decal.Number}\" texture=\"{outfit.Decal.Texture}\" />");
+            WriteLine($"<Component id=\"11\" drawable=\"{outfit.Top.Number}\" texture=\"{outfit.Top.Texture}\" />");
+            WriteLine($"</Components>");
+            WriteLine($"</Variation>");
+
+            ReadKey();
+        }
+    }
+}
